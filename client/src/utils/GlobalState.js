1f04a4ae98f8b1fe257b81b1ed3a004ab0cc1f08
German Ramirez <mitsuominagi@gmail.com>
2021-08-12 21:03:14 -0500

    Edited the template to have activites for the initial state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -6,7 +6,7 @@ const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
-    products: [],
+    activities: [],
     cart: [],
     cartOpen: false,
     categories: [],
