49fd3ce53ebdb40f697188786ce2ab1241b71b62
Adina Henderson <adinahenderson.mail@gmail.com>
2021-08-13 11:57:35 -0400

    populated seeds with new categories, added places and awaiting images to include them

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,151 +1,163 @@
 const db = require('./connection');
-const { User, Product, Category } = require('../models');
+const { User, Activity, Category } = require('../models');
 
 db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: "Beach" },
+    { name: "Night Life" },
+    { name: "Rentals" },
+    { name: "Spa" },
   ]);
 
   console.log('categories seeded');
 
-  await Product.deleteMany();
+  await Activity.deleteMany();
 
-  const products = await Product.insertMany([
+  const activities = await Activity.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Snorkeling',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+        'This activity is perfect for adults and children, under the instruction of professionals with clean and quality equipment. Prices are per person',
+      image: '#',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      place: 'CANCUN',
+      price: 65.00,
+
+      quantity: 2
     },
     {
-      name: 'Canned Coffee',
+      name: 'Night Cruise',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
-      category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
-    },
-    {
-      name: 'Toilet Paper',
+        'Join us at our night cruise, departing at 6pm,where you can enjoy dinner with the most prestrigious chefs. After dinner, come to the dancefloor on the deck of the ship',
+      image: '#',
       category: categories[1]._id,
-      description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
-      price: 7.99,
-      quantity: 20
+      place: 'HUATULCO',
+      price: 35.00,
+      quantity: 5
     },
     {
-      name: 'Handmade Soap',
-      category: categories[1]._id,
-      description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
-    },
-    {
-      name: 'Set of Wooden Spoons',
-      category: categories[1]._id,
-      description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
-      price: 14.99,
-      quantity: 100
-    },
-    {
-      name: 'Camera',
+      name: 'Banana',
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+        'Have fun while riding the banana, .',
+      image: '#',
+      place: 'ACAPULCO',
+      price: 20.50,
+      quantity: 1
     },
     {
-      name: 'Tablet',
-      category: categories[2]._id,
-      description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
-    },
-    {
-      name: 'Tales at Bedtime',
+      name: 'Massage',
       category: categories[3]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
+        'Relax and let go with our special massage packages. Our service includes aromatherapy, ice-cold and hot sauna, green detoxing tea, finishing with our award winning massage with hot stones.',
+      image: '#',
+      price: 70.00,
+      place: 'CABO',
+      quantity: 1
+    },
+    {
+      name: 'Bicycles',
+      category: categories[2]._id,
+      description:
+        'Enjoy a ride in our beaches with our environmental friendly bikes.',
+      image: '#',
+      price: 14.99,
+      place: 'CANCUN',
+      quantity: 3
+    },
+    {
+      name: 'Scuba diving',
+      category: categories[0]._id,
+      description:
+        'Discover nature and its beaute under the sea, with our quality equipment. ',
+      image: '#',
+      price: 30,
+      place: 'CABO',
+      quantity: 2
+    },
+    {
+      name: 'Kayak',
+      category: categories[2]._id,
+      description:
+        'What better activity than kayaking your way into the ocean and getting that extra exercise. Rentals available',
+      image: '#',
+      price: 25.00,
+      place: 'PUERTO VALLARTA',
+      quantity: 1
+    },
+    {
+      name: 'Parasailing',
+      category: categories[0]._id,
+      description:
+        'See the beauty of the ocean from above! ',
       image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
+      price: 35.00,
+      place: 'CANCUN',
+      quantity: 1
     },
     {
-      name: 'Spinning Top',
-      category: categories[4]._id,
+      name: 'Sailing',
+      category: categories[0]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
+      image: '#',
+      price: 100.00,
+      place: 'CABO',
+      quantity: 1
     },
     {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
+      name: 'Horse riding',
+      category: categories[2]._id,
       description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
+        'Enjoy a ride in your favorite horse along the beach, explore nature!.',
       image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
+      place: 'PUERTO VALLARTA',
+      price: 40.00,
+      quantity: 3
     },
     {
-      name: 'Teddy Bear',
-      category: categories[4]._id,
+      name: 'Massage in a tent',
+      category: categories[3]._id,
       description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
+        'Enjoy the sound of the ocean waves while resting with a full body massage. Organic oils and aromatherapy to make you dream.',
+      image: '#',
+      price: 95.00,
+      place: 'CANCUN',
+      quantity: 1
     },
     {
-      name: 'Alphabet Blocks',
-      category: categories[4]._id,
+      name: 'Swim with dolphis',
+      category: categories[0]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
+        'Adventure to swim with friendly mammals. Swimming with dolphins is not only fun, but they help you heal!.',
+      image: '#',
+      place: 'HUATULCO',
+      price: 45,
+      quantity: 2
     }
   ]);
 
-  console.log('products seeded');
+  console.log('activities seeded');
 
   await User.deleteMany();
 
   await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
+    firstName: 'Juanita',
+    lastName: 'Tequilera',
+    email: 'juanita@prueba.com',
     password: 'password12345',
     orders: [
       {
-        products: [products[0]._id, products[0]._id, products[1]._id]
+        activities: [activities[0]._id, activities[0]._id, activities[1]._id]
       }
     ]
   });
 
   await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
+    firstName: 'Fausto',
+    lastName: 'Kim',
+    email: 'fkim@prueba.com',
     password: 'password12345'
   });
 
