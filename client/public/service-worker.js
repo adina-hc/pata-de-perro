92e54ddfea595274fbc077aa8d037f5815a83959
German Ramirez <mitsuominagi@gmail.com>
2021-08-20 09:25:56 -0500

    manifest

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,16 +1,3 @@
-this.addEventListener('message', async (event) => {
+self.addEventListener('message', async (event) => {
     console.log('Got message in the service worker', event);
 });
-this.addEventListener('fetch', function(event) {
-    event.respondWith(async function() {
-       try{
-         var res = await fetch(event.request);
-         var cache = await caches.open('cache');
-         cache.put(event.request.url, res.clone());
-         return res;
-       }
-       catch(error){
-         return caches.match(event.request);
-        }
-      }());
-  });
