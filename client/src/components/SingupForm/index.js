b7d75c8134e0bd960dae6eaeb6a3979df437d0b2
Adina Henderson <adinahenderson.mail@gmail.com>
2021-08-20 15:59:13 -0400

    corrected wave on mobile devices

--- client/src/components/SingupForm/index.js
+++ client/src/components/SingupForm/index.js
@@ -47,24 +47,28 @@ ${multipleStyles};
 `;
 
 const StyledButton = styled.button`
-display: inline;
-width: 50%;
-padding: 12px 0;
-font-family: inherit;
-font-size: 14px;
-font-weight: 700;
-color: #fff;
-background-color: #a2ccb6;
-border: 0;
-border-radius: 35px;
-box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
-cursor: pointer;
-transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+  display: inline;
+  width: 50%;
+  padding: 12px 0;
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 700;
+  color: #fff;
+  background-color: #a2ccb6;
+  border: 0;
+  border-radius: 35px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
 
-&:hover {
-  box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
-  transform: translate(0, -5px);
-}
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.16);
+    transform: translate(0, -5px);
+  }
+  @media (max-width: 700px) {
+    width: 100%;
+    margin-bottom: 10%;
+  }
 `;
 
 
@@ -91,6 +95,8 @@ transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
 a{
   color:white
 }
+@media (max-width: 700px) {
+  width: 100%;
 `;
 
 
