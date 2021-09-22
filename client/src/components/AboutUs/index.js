6804f4e36d2ecd33a7c60761a6d24edc50830744
German Ramirez <mitsuominagi@gmail.com>
2021-09-21 21:24:10 -0500

    Fixed error

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -17,13 +17,13 @@ const AboutUs = () => {
                         <GitHubLink  href="https://github.com/vicvallejo" target="_blank" rel="noreferrer">
                             <GitHubButton>Victor 🐚</GitHubButton>
                         </GitHubLink>
-                        <GitHubLink  href="https://github.com/adina-hc" target="_blank" rel="noreferrer">
+                        <GitHubLink  href="https://github.com/AdrianoArmen" target="_blank" rel="noreferrer">
                             <GitHubButton>Jorge 🌴</GitHubButton>
                         </GitHubLink>
                         <GitHubLink  href="https://github.com/jorgeatcabo" target="_blank" rel="noreferrer">
                             <GitHubButton>Adriano 🏄</GitHubButton>
                         </GitHubLink>
-                        <GitHubLink  href="https://github.com/AdrianoArmen" target="_blank" rel="noreferrer">
+                        <GitHubLink  href="https://github.com/adina-hc/pata-de-perro" target="_blank" rel="noreferrer">
                             <GitHubButton>Repository 🦀</GitHubButton>
                         </GitHubLink>
                     </GitHubList>
